5b35f9646fe005be9b62d9c83917fd8332add727
Sug4chy <sb.9226981433@gmail.com>
2023-11-05 21:06:21 +0500

    re-do task 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,20 +3,18 @@ package org.example;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    static int x = 0;
+    static final AtomicInteger x = new AtomicInteger(0);
     static final AtomicInteger count = new AtomicInteger(0);
 
     static class LuckyThread extends Thread {
         @Override
         public void run() {
-            synchronized (count) {
-                while (x < 999999) {
-                    x++;
-                    if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
-                            % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
-                        System.out.println(x);
-                        count.incrementAndGet();
-                    }
+            while (x.intValue() < 999999) {
+                int intValue = x.incrementAndGet();
+                if ((intValue % 10) + (intValue / 10) % 10 + (intValue / 100) % 10 == (intValue / 1000)
+                        % 10 + (intValue / 10000) % 10 + (intValue / 100000) % 10) {
+                    System.out.println(intValue);
+                    count.incrementAndGet();
                 }
             }
         }
